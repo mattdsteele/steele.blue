f2eb1936a6443dd732686dece29e8cb47d440480
Matt Steele <orphum@gmail.com>
2013-10-19 23:43:35 -0500

    Deploy to S3

--- Gruntfile.js
+++ Gruntfile.js
@@ -47,13 +47,37 @@ module.exports = function(grunt) {
         src: '<%= config.root %>/css/main.css',
         dest: '<%= config.root %>/css/main.css'
       }
+    },
+    aws: grunt.file.readJSON('grunt-aws.json'),
+    aws_s3: {
+      options: {
+        accessKeyId: '<%= aws.key %>',
+        secretAccessKey: '<%= aws.secret %>',
+        region: '<%= aws.region %>',
+        bucket: '<%= aws.bucket %>'
+      },
+      blog: {
+        options: {
+          debug: false
+        },
+        files: [
+          { dest: '/', action: 'delete' },
+          {
+            cwd: '<%= config.root %>/_site',
+            src: ['**'],
+            dest: '',
+            expand: true
+          }
+        ]
+      }
     }
   });
 
   // These plugins provide necessary tasks.
   // Default task.
-  grunt.registerTask('default', []);
+  grunt.registerTask('default', ['content']);
   grunt.registerTask('content', ['less:assets', 'autoprefixer', 'shell:jekyll']);
   grunt.registerTask('dev', ['connect:server', 'watch']);
+  grunt.registerTask('deploy', ['content', 'aws_s3']);
 
 };
