a4b4fa6be61c1cc42d9a5b5d1d960874a3647d6f
Matt Steele <orphum@gmail.com>
2017-03-26 20:54:13 -0500

    Remove HTML cache from SW

--- blog/_assets/js/sw.js
+++ blog/_assets/js/sw.js
@@ -1,6 +1,8 @@
+const version = '2';
+
 importScripts('js/sw-toolbox.js');
 
-toolbox.router.get(/(.*)\/?/, toolbox.cacheFirst, {});
+// toolbox.router.get(/(.*)\/?/, toolbox.cacheFirst, {});
 toolbox.router.get(/.*\.(gif|png|jpg|svg)$/, toolbox.cacheFirst, {});
 
 toolbox.router.get(/^https:\/\/fonts.googleapis.com/, toolbox.cacheFirst, {});
