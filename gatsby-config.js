f5edc007dc000211b226736210290f197c475d3d
Dustin Schau <dustinschau@gmail.com>
2018-12-05 10:35:56 -0600

    refactor: migrate to gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,56 @@
+const title = 'steele.blue'
+
+module.exports = {
+  siteMetadata: {
+    title,
+    author: 'Matt Steele',
+    url: 'http://steele.blue',
+    description: 'The personal website of Matt Steele'
+  },
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `blog`,
+        path: `${__dirname}/content/blog`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/content/images`,
+      },
+    },
+    'gatsby-transformer-sharp',
+    {
+      resolve: 'gatsby-transformer-remark',
+      options: {
+        plugins: [
+          'gatsby-remark-responsive-iframe',
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              maxWidth: 600,
+            },
+          },
+          'gatsby-remark-prismjs'
+        ]
+      }
+    },
+    'gatsby-plugin-sharp',
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: title,
+        short_name: 'starter',
+        start_url: '/',
+        background_color: '#663399',
+        theme_color: '#663399',
+        display: 'minimal-ui',
+        icon: 'src/images/gatsby-icon.png',
+      },
+    },
+  ],
+}
---
+++ src/pages/index.js
@@ -0,0 +1,45 @@
+import React from 'react'
+import { graphql, Link } from 'gatsby'
+
+import Layout from '../components/layout'
+import Post from '../components/post'
+
+export default function IndexPage({ data }) {
+  const [latest] = data.posts.edges
+  return (
+    <Layout>
+      <Post {...latest.node} />
+      <h2>All posts</h2>
+      <ul>
+        {
+          data.posts.edges.map(({ node }) => (
+            <li key={node.fields.slug}>
+              <Link to={node.fields.slug}>{node.frontmatter.title}</Link>
+              {` - ${node.fields.dateWithYear}`}
+            </li>
+          ))
+        }
+      </ul>
+    </Layout>
+  )
+}
+
+export const indexQuery = graphql`
+  query IndexQuery {
+    posts:allMarkdownRemark(sort:{fields:[fields___date], order:DESC}) {
+      edges {
+        node {
+          fields {
+            dateWithDay:date(formatString:"DD MMM YYYY")
+            dateWithYear:date(formatString:"MMM YYYY")
+            slug
+          }
+          frontmatter {
+            title
+          }
+          html
+        }
+      }
+    }
+  }
+`
