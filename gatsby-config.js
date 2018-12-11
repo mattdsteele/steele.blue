c8b31d9cb4cf17b98a2fe7f61d5c18edeb3cd6dd
mattdsteele <orphum@gmail.com>
2018-12-10 20:53:13 -0600

    Use Youtube gatsby plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -36,6 +36,11 @@ module.exports = {
       resolve: 'gatsby-transformer-remark',
       options: {
         plugins: [
+          {
+            resolve: 'gatsby-remark-embed-youtube',
+            options: { width: 160, height: 90 },
+          },
+
           'gatsby-remark-responsive-iframe',
           {
             resolve: `gatsby-remark-images`,
