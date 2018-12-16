94e34f716afc61196c1265a6960670ca1900f755
Dustin Schau <dustinschau@gmail.com>
2018-12-16 15:57:26 -0600

    fix: embed speakerdeck as iframe instead of js embed

--- plugins/gatsby-remark-embed-speakerdeck/index.js
+++ plugins/gatsby-remark-embed-speakerdeck/index.js
@@ -1,13 +1,24 @@
-const visit = require('unist-util-visit')
-const sd = (_, id) => {
-  return `<script async class="speakerdeck-embed" data-id="${id}" data-ratio="1.77777777777778" src="https://speakerdeck.com/assets/embed.js"></script>`
+const visit = require('unist-util-visit');
+
+const speakerDeckEmbed = id => {
+  const props = [
+    `src="//speakerdeck.com/player/${id}"`,
+    `allowfullscreen`,
+    `scrolling="no"`,
+    `allow="autoplay; encrypted-media"`,
+    `width="600"`,
+    `height="400"`
+  ]
+
+  return `<iframe ${props.join(' ')}></iframe>`;
 }
+
 module.exports = ({ markdownAST }) => {
   visit(markdownAST, `inlineCode`, node => {
     if (node.value.startsWith('speakerdeck')) {
       const [_, id] = node.value.split(':')
       node.type = 'html'
-      node.value = sd`${id}`
+      node.value = speakerDeckEmbed(id)
     }
   })
   return markdownAST
