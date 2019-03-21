aa5ecfa4c77038f4b1354a9f61c66bcc347abc82
mattdsteele <orphum@gmail.com>
2019-03-20 22:51:09 -0500

    Add GA

--- gatsby-config.js
+++ gatsby-config.js
@@ -132,5 +132,17 @@ module.exports = {
         ],
       },
     },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: 'UA-30572618-1',
+        // Puts tracking script in the head instead of the body
+        head: false,
+        // Setting this parameter is optional
+        anonymize: true,
+        // Setting this parameter is also optional
+        respectDNT: true,
+      },
+    },
   ],
 };
