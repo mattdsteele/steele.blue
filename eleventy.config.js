91853b5fbc12c01cff362fc087508b479cf6a1be
mattdsteele <orphum@gmail.com>
2024-10-07 07:53:26 -0500

    RSS Club

--- eleventy.config.js
+++ eleventy.config.js
@@ -2,4 +2,11 @@
 export default async function(eleventyConfig) {
     eleventyConfig.setIncludesDirectory('src');
     eleventyConfig.addLayoutAlias('post', 'layouts/post.njk');
+    
+    // RSS Only
+    eleventyConfig.addCollection('nonRssPosts', function (collectionApi) {
+      return collectionApi.getFilteredByGlob('content/blog/**.md').filter((item) => {
+        return item.data.rss_only !== true;
+    });
+    });
 }
