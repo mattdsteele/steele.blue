9b13e198f95aa1131a475b377a8ec95db7a45249
mattdsteele <orphum@gmail.com>
2024-10-18 22:10:31 -0500

    Add images

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,12 +1,30 @@
+import {eleventyImageTransformPlugin} from '@11ty/eleventy-img';
 
-export default async function(eleventyConfig) {
-    eleventyConfig.setIncludesDirectory('src');
-    eleventyConfig.addLayoutAlias('post', 'layouts/post.njk');
-    
-    // RSS Only
-    eleventyConfig.addCollection('nonRssPosts', function (collectionApi) {
-      return collectionApi.getFilteredByGlob('content/blog/**.md').filter((item) => {
-        return item.data.rss_only !== true;
-    });
-    });
+export default async function (eleventyConfig) {
+  eleventyConfig.setIncludesDirectory('src');
+  eleventyConfig.addLayoutAlias('post', 'layouts/post.njk');
+
+  // RSS Only
+  eleventyConfig.addCollection('nonRssPosts', function (collectionApi) {
+    return collectionApi
+      .getFilteredByGlob('content/blog/**.md')
+      .filter((item) => {
+        return !item.data.rss_only;
+      });
+  });
+
+  // TODO write a converter
+  eleventyConfig.amendLibrary('md', mdLib => {
+    // mdLib.use(youtubeLitePlugin);
+  })
+
+  eleventyConfig.addPlugin(eleventyImageTransformPlugin, {
+    extensions: 'html',
+    formats: ['jpg'],
+    widths: [600, 1000],
+    defaultAttributes: {
+      sizes: '100vw',
+      loading: 'lazy',
+    },
+  });
 }
