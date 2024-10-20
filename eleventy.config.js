67457d39caa8032bcb00b6208db537f4eaba6fc0
mattdsteele <orphum@gmail.com>
2024-10-20 18:55:39 -0500

    Initial syntax highlighting

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,6 +1,9 @@
-import {eleventyImageTransformPlugin} from '@11ty/eleventy-img';
+import { eleventyImageTransformPlugin } from '@11ty/eleventy-img';
 import webcPlugin from '@11ty/eleventy-plugin-webc';
-import { EleventyRenderPlugin } from "@11ty/eleventy";
+import { EleventyRenderPlugin } from '@11ty/eleventy';
+import { createHash } from 'node:crypto';
+import { resolve } from 'node:path';
+import { FlatCache } from 'flat-cache';
 
 export default async function (eleventyConfig) {
   eleventyConfig.setIncludesDirectory('src');
@@ -16,12 +19,14 @@ export default async function (eleventyConfig) {
   });
 
   // TODO write a converter
-  eleventyConfig.amendLibrary('md', mdLib => {
+  eleventyConfig.amendLibrary('md', (mdLib) => {
     // mdLib.use(youtubeLitePlugin);
-  })
+  });
 
   eleventyConfig.addPlugin(EleventyRenderPlugin);
-  eleventyConfig.addPlugin(webcPlugin, { components: 'src/components/**/*.webc'});
+  eleventyConfig.addPlugin(webcPlugin, {
+    components: 'src/components/**/*.webc',
+  });
   eleventyConfig.addPlugin(eleventyImageTransformPlugin, {
     extensions: 'html',
     formats: ['jpg'],
@@ -30,5 +35,61 @@ export default async function (eleventyConfig) {
       sizes: '100vw',
       loading: 'lazy',
     },
+  });
+
+  // syntax highlighting
+  // via https://www.hoeser.dev/blog/2023-02-07-eleventy-shiki-simple/
+  // and https://stefanzweifel.dev/posts/2024/06/03/how-i-use-shiki-in-eleventy/
+  // empty call to notify 11ty that we use this feature
+  // eslint-disable-next-line no-empty-function
+  eleventyConfig.amendLibrary('md', () => {});
+
+  eleventyConfig.on('eleventy.before', async () => {
+    const shiki = await import('shiki');
+
+    const highlighter = await shiki.createHighlighter({
+      themes: ['dark-plus'],
+      theme: 'dark-plus',
+      langs: [
+        'js',
+        'html',
+        'bash',
+        'sql',
+        'jsx',
+        'yaml',
+        'ts',
+        'dockerfile',
+        'xml',
+        'go',
+        'c',
+        'c++',
+        'python',
+      ],
+      langAlias: {
+        Dockerfile: 'dockerfile',
+        'c++': 'cpp',
+      },
+    });
+    const cache = new FlatCache({cacheDir: resolve('.cache/shiki')});
+    eleventyConfig.amendLibrary('md', (mdLib) =>
+      mdLib.set({
+        highlight: (code, lang) => {
+          const hash = createHash('md5').update(code).digest('hex');
+          const cachedValue = cache.getKey(hash);
+
+          if (cachedValue) {
+            return cachedValue;
+          }
+
+          const html = highlighter.codeToHtml(code, {
+            lang,
+            themes: { light: 'dark-plus', dark: 'dark-plus' },
+          });
+          cache.setKey(hash, html);
+          cache.save();
+          return html;
+        },
+      })
+    );
   });
 }
