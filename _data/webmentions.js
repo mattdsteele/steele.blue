a3ec0dfffb8076c6527740ce90170040a4b70c05
mattdsteele <orphum@gmail.com>
2025-04-16 20:18:41 -0500

    Add cached WM data

--- _data/webmentions.js
+++ _data/webmentions.js
@@ -1,13 +1,22 @@
 import EleventyFetch from '@11ty/eleventy-fetch';
 import { groupBy } from 'lodash-es';
 
+import {readFileSync} from 'node:fs';
+const wm1 = JSON.parse(readFileSync('_data/webmention-1.json'));
+
 const apiKey = process.env.WEBMENTION_IO_API;
 
 const url = `https://webmention.io/api/mentions.jf2?token=${apiKey}&per-page=1000`;
 
 export default async () => {
-  const data = await EleventyFetch(url, { duration: '1d', type: 'json' });
-  const grouped = groupBy(data.children, 'wm-target');
+  // This first set of data is from a lost webmention.io account
+  const data = wm1;
+
+  // Then, concat it with the current webmention.io account
+  const wmData = await EleventyFetch(url, { duration: '1d', type: 'json' });
+  const allWms = wmData.children.concat(data.children);
+
+  const grouped = groupBy(allWms, 'wm-target');
   const baseData = {};
   for (const key of Object.keys(grouped)) {
     baseData[content(key)] = grouped[key];
