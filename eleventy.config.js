5d6c66e3afcefd9568b38f0900aebd309eaf68c6
mattdsteele <orphum@gmail.com>
2025-09-14 09:49:55 -0500

    Rewrite components in Vento

--- eleventy.config.js
+++ eleventy.config.js
@@ -3,6 +3,7 @@ import { eleventyImageTransformPlugin } from '@11ty/eleventy-img';
 import { feedPlugin } from '@11ty/eleventy-plugin-rss';
 import webcPlugin from '@11ty/eleventy-plugin-webc';
 import { inspect } from 'node:util';
+import { VentoPlugin } from 'eleventy-plugin-vento';
 
 import webmentionPlugin from './src/webmention-plugin.js';
 import shikiHighlightingPlugin from './src/shiki-highlighting-plugin.js';
@@ -10,7 +11,7 @@ import asciinemaPlugin from './src/plugins/11ty-asciinema-plugin.js';
 
 export default async function (eleventyConfig) {
   eleventyConfig.setIncludesDirectory('src');
-  eleventyConfig.addLayoutAlias('post', 'layouts/post.webc');
+  eleventyConfig.addLayoutAlias('post', 'layouts/post.vto');
 
   eleventyConfig.addPassthroughCopy({static: '/'});
   eleventyConfig.addPassthroughCopy({
@@ -29,7 +30,21 @@ export default async function (eleventyConfig) {
       });
   });
 
+  eleventyConfig.addPlugin(webmentionPlugin);
+  eleventyConfig.addPlugin(VentoPlugin);
   eleventyConfig.addPlugin(EleventyRenderPlugin);
+
+  eleventyConfig.addPairedShortcode('ventoRender', async function (content, ext, context = {}) {
+      const renderer = eleventyConfig.javascript.functions.renderContent.bind(this);
+      const rendered = await renderer(
+        content,
+        ext,
+        { ...(this.ctx || {}), ...context }
+      );
+      return rendered;
+    }
+  );
+
   eleventyConfig.addPlugin(asciinemaPlugin);
   eleventyConfig.addPlugin(webcPlugin, {
     components: 'src/components/**/*.webc',
@@ -48,6 +63,14 @@ export default async function (eleventyConfig) {
   eleventyConfig.addPlugin(shikiHighlightingPlugin);
 
 
+  eleventyConfig.addFilter('date', (str, format) => {
+    // currently only returns format = '%b %d, %Y'
+    return new Date(str).toLocaleDateString('en-US', {
+      month: 'short',
+      day: 'numeric',
+      year: 'numeric',
+    });
+  });
   eleventyConfig.addPlugin(feedPlugin, {
     type: 'atom',
     outputPath: '/atom.xml',
@@ -78,6 +101,5 @@ export default async function (eleventyConfig) {
     }
     return `${num} ${plural || `${singular}s`}`;
   });
-  eleventyConfig.addPlugin(webmentionPlugin);
   eleventyConfig.addBundle("css");
 }
